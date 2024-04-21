293c0db3baf28ef7646e0f5dd5c445caf3872524
Somaya Mohamed <118680203+somiamohamed@users.noreply.github.com>
2024-04-21 20:05:18 +0200

    Add files via upload

---
+++ LinearSearchImplementation/LinearSearchImplementation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 5, 3, 8, 4, 2 };
+        int target = 4;
+        int index = LinearSearch(arr, target);
+
+        if (index != -1)
+        {
+            Console.WriteLine("Element found at index: " + index);
+        }
+        else
+        {
+            Console.WriteLine("Element not found.");
+        }
+    }
+
+    public static int LinearSearch<T>(T[] arr, T target, IComparer<T> comparer = null) where T : IComparable
+    {
+        if (comparer == null)
+        {
+            comparer = Comparer<T>.Default;
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (comparer.Compare(arr[i], target) == 0)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
